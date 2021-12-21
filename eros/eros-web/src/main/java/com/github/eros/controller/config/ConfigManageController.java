db49d564ef145ecf76e5676a5df723d8319597d9
“孙浩” <sunhao7@xiaomi.com>
2021-12-21 22:49:01 +0800

    feat:测试

--- eros/eros-web/src/main/java/com/github/eros/controller/config/ConfigManageController.java
+++ eros/eros-web/src/main/java/com/github/eros/controller/config/ConfigManageController.java
@@ -1,6 +1,7 @@
 package com.github.eros.controller.config;
 
 
+import com.github.eros.common.model.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfigManageController {
 
     @GetMapping("/demo")
-    public String demo(){
-        return "demo";
+    public Result demo(){
+        Result<Object> result = Result.createSuccess();
+        result.setData("demo");
+        return result;
     }
 }
